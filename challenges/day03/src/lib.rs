4494180a4b7a22750864a62698f93f1b96fa4fd8
Conrad Ludgate <conradludgate@gmail.com>
2023-12-03 09:24:03 +0000

    day3p2

--- challenges/day03/src/lib.rs
+++ challenges/day03/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, process::Output};
+use std::{collections::HashSet, fmt::Display, process::Output};
 
 use aoc::{Challenge, Parser as ChallengeParser};
 use nom::{bytes::complete::tag, IResult, Parser};
@@ -13,6 +13,7 @@ pub struct Solution {
 enum State {
     None,
     Num(usize),
+    Gear,
     Symbol,
 }
 
@@ -27,6 +28,8 @@ impl ChallengeParser for Solution {
                     output.push(State::Num((b - b'0') as usize));
                 } else if *b == b'.' {
                     output.push(State::None);
+                } else if *b == b'*' {
+                    output.push(State::Gear);
                 } else {
                     output.push(State::Symbol);
                 }
@@ -52,25 +55,67 @@ impl Challenge for Solution {
         let mut is_next_to = false;
         for (i, state) in self.states.iter().enumerate() {
             match state {
-                State::None | State::Symbol => {
+                State::None | State::Symbol | State::Gear => {
                     if is_next_to {
                         sum += current_num;
                         is_next_to = false;
                     }
                     current_num = 0;
-                },
+                }
                 State::Num(s) => {
                     current_num *= 10;
                     current_num += s;
                     is_next_to |= self.is_next_to_symbol(i);
-                },
+                }
             }
         }
         sum
     }
 
     fn part_two(self) -> impl Display {
-        0
+        let mut spots = HashSet::new();
+        let mut gears: Vec<GearRatio> = vec![GearRatio::One(0); self.states.len()];
+        let mut current_num = 0;
+        for (i, state) in self.states.iter().enumerate() {
+            match state {
+                State::None | State::Symbol | State::Gear => {
+                    for spot in spots.drain() {
+                        // why is this necessary????
+                        let gear: &mut GearRatio = &mut gears[spot];
+                        gear.insert(current_num);
+                    }
+                    current_num = 0;
+                }
+                State::Num(s) => {
+                    current_num *= 10;
+                    current_num += s;
+                    self.is_next_to_gear(i, &mut spots);
+                }
+            }
+        }
+        gears.into_iter().map(GearRatio::eval).sum::<usize>()
+    }
+}
+
+#[derive(Clone, Copy)]
+enum GearRatio {
+    One(usize),
+    Two(usize),
+}
+
+impl GearRatio {
+    fn insert(&mut self, x: usize) {
+        *self = match *self {
+            GearRatio::One(0) => GearRatio::One(x),
+            GearRatio::One(y) => GearRatio::Two(x * y),
+            GearRatio::Two(_) => GearRatio::Two(0),
+        }
+    }
+    fn eval(self) -> usize {
+        match self {
+            GearRatio::One(_) => 0,
+            GearRatio::Two(x) => x,
+        }
     }
 }
 
@@ -111,7 +156,43 @@ impl Solution {
     }
 
     fn check(&self, pos: usize) -> bool {
-        matches!(self.states[pos], State::Symbol)
+        matches!(self.states[pos], State::Symbol | State::Gear)
+    }
+
+    fn is_next_to_gear(&self, pos: usize, spaces: &mut HashSet<usize>) {
+        let left = pos % self.width > 0;
+        let right = pos % self.width + 1 < self.width;
+        let up = pos >= self.width;
+        let down = pos + self.width < self.states.len();
+
+        if left && self.check_gear(pos - 1) {
+            spaces.insert(pos - 1);
+        }
+        if right && self.check_gear(pos + 1) {
+            spaces.insert(pos + 1);
+        }
+        if up && self.check_gear(pos - self.width) {
+            spaces.insert(pos - self.width);
+        }
+        if down && self.check_gear(pos + self.width) {
+            spaces.insert(pos + self.width);
+        }
+        if left && up && self.check_gear(pos - 1 - self.width) {
+            spaces.insert(pos - 1 - self.width);
+        }
+        if right && down && self.check_gear(pos + 1 + self.width) {
+            spaces.insert(pos + 1 + self.width);
+        }
+        if left && down && self.check_gear(pos - 1 + self.width) {
+            spaces.insert(pos - 1 + self.width);
+        }
+        if right && up && self.check_gear(pos + 1 - self.width) {
+            spaces.insert(pos + 1 - self.width);
+        }
+    }
+
+    fn check_gear(&self, pos: usize) -> bool {
+        matches!(self.states[pos], State::Gear)
     }
 }
 
@@ -147,6 +228,6 @@ mod tests {
     #[test]
     fn part_two() {
         let output = Solution::parse(INPUT).unwrap().1;
-        assert_eq!(output.part_two().to_string(), "0");
+        assert_eq!(output.part_two().to_string(), "467835");
     }
 }
