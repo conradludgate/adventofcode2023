e492d352e75802425411d08c1536acd108a52f70
Conrad Ludgate <conradludgate@gmail.com>
2023-12-08 09:40:33 +0000

    array is faster

--- challenges/day08/src/lib.rs
+++ challenges/day08/src/lib.rs
@@ -1,19 +1,52 @@
-use std::{collections::BTreeMap, fmt::Display};
+use std::{
+    collections::BTreeMap,
+    fmt::{self, Display},
+};
 
 use aoc::{Challenge, Parser as ChallengeParser};
 use nom::IResult;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct Solution {
     steps: &'static [u8],
-    paths: BTreeMap<u16, [u16; 2]>,
+    paths: Box<[[u16; 2]; 65536]>,
+}
+
+impl fmt::Debug for Solution {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        struct MapSlice<'a>(&'a [[u16; 2]; 65536]);
+        impl fmt::Debug for MapSlice<'_> {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                let mut list = f.debug_map();
+                for (i, &[a, b]) in self.0.iter().enumerate() {
+                    if [a, b] == [0, 0] {
+                        continue;
+                    }
+
+                    list.entry(
+                        &std::str::from_utf8(&unmap(i as u16)).unwrap(),
+                        &[
+                            std::str::from_utf8(&unmap(a)).unwrap(),
+                            std::str::from_utf8(&unmap(b)).unwrap(),
+                        ],
+                    );
+                }
+                list.finish()
+            }
+        }
+
+        f.debug_struct("Solution")
+            .field("steps", &self.steps)
+            .field("paths", &MapSlice(&self.paths))
+            .finish()
+    }
 }
 
 impl ChallengeParser for Solution {
     fn parse(input: &'static str) -> IResult<&'static str, Self> {
         let (steps, rest) = input.split_once('\n').unwrap();
         let mut input = rest.as_bytes();
-        let mut paths = BTreeMap::new();
+        let mut paths: Box<[[u16; 2]; 65536]> = vec![[0, 0]; 65536].try_into().unwrap();
         while input.len() > 17 {
             let (line, r) = input.split_at(17);
             input = r;
@@ -22,7 +55,7 @@ impl ChallengeParser for Solution {
             let left = elem(line[8..11].try_into().unwrap());
             let right = elem(line[13..16].try_into().unwrap());
 
-            paths.insert(start, [left, right]);
+            paths[start as usize] = [left, right];
         }
 
         Ok((
@@ -39,6 +72,12 @@ const fn elem(x: [u8; 3]) -> u16 {
     let [a, b, c] = x;
     ((c as u16 & 0x1f) << 10) | ((b as u16 & 0x1f) << 5) | (a as u16 & 0x1f)
 }
+const fn unmap(x: u16) -> [u8; 3] {
+    let c = (x >> 10) as u8 | 0x40;
+    let b = ((x >> 5) & 0x1f) as u8 | 0x40;
+    let a = (x & 0x1f) as u8 | 0x40;
+    [a, b, c]
+}
 
 const fn lr(x: u8) -> u8 {
     (x >> 4) & 0x1
@@ -53,7 +92,7 @@ impl Challenge for Solution {
         let mut i = 0;
         while state != GOAL {
             let step = lr(self.steps[i % self.steps.len()]);
-            state = self.paths[&state][step as usize];
+            state = self.paths[state as usize][step as usize];
             i += 1;
         }
         i
@@ -62,12 +101,17 @@ impl Challenge for Solution {
     fn part_two(self) -> impl Display {
         let mut x = 1;
 
-        for (&state, _) in self.paths.range(elem(*b"AAA")..=elem(*b"ZZA")) {
-            let mut state = state;
+        for (mut state, path) in self.paths[1024..2048].iter().enumerate() {
+            if *path == [0, 0] {
+                continue;
+            }
+            // fix enumeration
+            state += 1024;
+
             let mut j = 0;
             while state >> 10 != 26 {
                 let step = lr(self.steps[j % self.steps.len()]);
-                state = self.paths[&state][step as usize];
+                state = self.paths[state][step as usize] as usize;
                 j += 1;
             }
             x = (x * j) / gcd(x, j)
@@ -154,6 +198,7 @@ XXX = (XXX, XXX)
             i += 1;
             assert_eq!(b & 0x1f, i);
         }
+        assert_eq!(elem(*b"XXZ") >> 10, 26);
 
         assert!(elem(*b"ZZA") < elem(*b"AAB"));
         // assert_eq!(elem(*b"ZZA"), (26<<10)+(26<<5)+26);
