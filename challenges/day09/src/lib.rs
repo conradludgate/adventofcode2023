b5abc6c11c365b5a92462cae7dec0bc127020e05
Conrad Ludgate <conradludgate@gmail.com>
2023-12-09 10:49:33 +0000

    use lagrange interpolation

--- challenges/day09/src/lib.rs
+++ challenges/day09/src/lib.rs
@@ -13,7 +13,7 @@ impl ChallengeParser for Solution {
     fn parse(mut input: &'static str) -> IResult<&'static str, Self> {
         let mut last = 0;
         let mut ranges = Vec::with_capacity(200);
-        let mut all = Vec::with_capacity(2000);
+        let mut all = Vec::with_capacity(200 * 21);
         while !input.is_empty() {
             loop {
                 let i = input.find([' ', '\n']).unwrap();
@@ -38,43 +38,160 @@ impl ChallengeParser for Solution {
 impl Challenge for Solution {
     const NAME: &'static str = env!("CARGO_PKG_NAME");
 
-    fn part_one(mut self) -> impl fmt::Display {
+    fn part_one(self) -> impl fmt::Display {
         self.ranges
             .into_iter()
-            .map(|r| predict(&mut self.all[r], 1, 0))
+            .map(|r| {
+                if r.len() == 21 {
+                    formula21(&self.all[r])
+                } else {
+                    formula6(&self.all[r])
+                }
+            })
             .sum::<i64>()
     }
 
-    fn part_two(mut self) -> impl fmt::Display {
+    fn part_two(self) -> impl fmt::Display {
         self.ranges
             .into_iter()
-            .map(|r| predict_back2(&mut self.all[r], 1, 0, 1))
+            .map(|r| formulam1(&self.all[r]))
             .sum::<i64>()
     }
 }
 
-fn predict(x: &mut [i64], j: usize, sum: i64) -> i64 {
-    let sum = sum + x[x.len() - 1];
-    for i in x.len() - j..x.len() {
-        x[i] -= x[i - 1];
+const fn l(x: i128, k: usize) -> i128 {
+    let mut l = 1;
+    let mut m = 0;
+    while m <= k {
+        l *= x - m as i128;
+        m += 1;
     }
-    if x[j..].iter().all(|x| *x == 0) {
-        sum
-    } else {
-        predict(x, j + 1, sum)
-    }
+    l
 }
 
-fn predict_back2(x: &mut [i64], j: usize, sum: i64, offset: i64) -> i64 {
-    let sum = sum + offset * x[0];
-    for i in (0..j).rev() {
-        x[i] = x[i + 1] - x[i];
+const L21: [i128; 21] = {
+    let mut out = [0; 21];
+    let mut k = 0;
+    while k <= 20 {
+        out[k] = l(21, k);
+        k += 1;
     }
-    if x[..x.len() - j].iter().all(|x| *x == 0) {
-        sum
-    } else {
-        predict_back2(x, j + 1, sum, -offset)
+    out
+};
+
+const LM: [i128; 21] = {
+    let mut out = [0; 21];
+    let mut k = 0;
+    while k <= 20 {
+        out[k] = l(-1, k);
+        k += 1;
     }
+    out
+};
+
+const L6: [i128; 6] = {
+    let mut out = [0; 6];
+    let mut k = 0;
+    while k <= 5 {
+        out[k] = l(6, k);
+        k += 1;
+    }
+    out
+};
+
+const BASES21: [i64; 21 * 21] = {
+    let mut out = [0; 21 * 21];
+    let mut k = 0;
+    while k <= 20 {
+        let l = L21[k];
+
+        let mut j = 0;
+        while j <= k {
+            let wj = lagrange_basis2(j as i64, k) as i128;
+            out[k * 21 + j] = (l / wj) as i64 / (21 - j as i64);
+            j += 1;
+        }
+        k += 1;
+    }
+    out
+};
+
+const BASES6: [i64; 6 * 6] = {
+    let mut out = [0; 6 * 6];
+    let mut k = 0;
+    while k <= 5 {
+        let l = L6[k];
+
+        let mut j = 0;
+        while j <= k {
+            let wj = lagrange_basis2(j as i64, k) as i128;
+            out[k * 6 + j] = (l / wj) as i64 / (6 - j as i64);
+            j += 1;
+        }
+        k += 1;
+    }
+    out
+};
+
+const BASESM1: [i64; 21 * 21] = {
+    let mut out = [0; 21 * 21];
+    let mut k = 0;
+    while k <= 20 {
+        let l = LM[k];
+
+        let mut j = 0;
+        while j <= k {
+            let wj = lagrange_basis2(j as i64, k) as i128;
+            out[k * 21 + j] = (l / wj) as i64 / (-1 - j as i64);
+            j += 1;
+        }
+        k += 1;
+    }
+    out
+};
+
+const fn lagrange_basis2(j: i64, k: usize) -> i64 {
+    let mut prod = 1;
+    let mut m = 0;
+    while m <= k as i64 {
+        if m == j {
+            m += 1;
+            continue;
+        }
+        prod *= j - m;
+        m += 1;
+    }
+    prod
+}
+
+fn formula21(ys: &[i64]) -> i64 {
+    let k = ys.len() - 1;
+
+    let mut sum = 0;
+    for j in 0..=k {
+        sum += ys[j] * BASES21[k * 21 + j];
+    }
+    sum
+}
+
+fn formula6(ys: &[i64]) -> i64 {
+    let k = ys.len() - 1;
+
+    let mut sum = 0;
+    for j in 0..=k {
+        sum += ys[j] * BASES6[k * 6 + j];
+    }
+    sum
+}
+
+fn formulam1(ys: &[i64]) -> i64 {
+    let k = ys.len() - 1;
+
+    let mut sum = 0;
+    for j in 0..=k {
+        sum += ys[j] * BASESM1[k * 21 + j];
+    }
+    sum
 }
 
 #[cfg(test)]
